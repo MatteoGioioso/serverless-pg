ac8538c575070acef1e22edfcff4546960ceda84
MatteoGioioso <gioioso.matteo@gmail.com>
2020-03-18 16:49:16 +0700

    chore: add delay in index.js test

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const ServerlessClient = require("./src");
 
-const connect = async i => {
-  const client = new ServerlessClient({
-    user: process.env.DB_USER,
-    host: "localhost",
-    database: process.env.DB_NAME,
-    password: "",
-    port: process.env.DB_PORT
+const _sleep = delay =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
   });
-  await client._sleep(Math.random()*60*1000);
-  console.log(i);
-  await client.sconnect();
-  await client.end();
-  return "connection ok";
+
+const execute = async i => {
+  await _sleep(Math.random()*60*250);
+
+  (async () => {
+    const client = new ServerlessClient({
+      user: process.env.DB_USER,
+      host: "localhost",
+      database: process.env.DB_NAME,
+      password: "",
+      port: process.env.DB_PORT
+    });
+
+    await client.sconnect();
+    const result = await client.query(`SELECT NOW()`);
+    console.log(result.rows[0].now, i);
+    await client.end();
+    return "connection ok";
+  })()
 };
 
 (async function () {
   const promiseArray = [];
   for (let i = 0; i < 1000; i++) {
-    promiseArray.push(connect(i))
+    promiseArray.push(execute(i))
   }
 
   await Promise.all(promiseArray)
