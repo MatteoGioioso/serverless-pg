f95dd09a688f00f7b7ff63f68cbcb4b22a988287
MatteoGioioso <gioioso.matteo@gmail.com>
2020-10-26 15:18:43 +0700

    feat: update main class
    - change to parameters instead of template string
    - change kill process query to ANY instead of one query per pid
    - add additional options
    - remove test db from query (left by mistake)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ function ServerlessClient(config) {
   Client.call(this, config);
 
   this._config = config;
-  this._maxRetries = 10;
+  this._maxRetries = config.maxRetries || 10;
+  this._maxConnections = config.maxConnections || 100;
+
+  // The bigger the slower, but more impactful on the connections dropped
+  this._maxRetrivedProcesses = config.maxRetrivedProcesses || 10;
+
+  // This number represent the percentage threshold at which the cleanup will be triggered.
+  // Ex: if you have 100 max connections and processesPercentageThreshold set at 60 (%),
+  // then it will start dropping connections if the total idle connections count is more than 60
+  this._processesPercentageThreshold = (config.processesPercentageThreshold || 50) / 100
   this._retries = 1;
 
   // pg throws an error if we terminate the connection, therefore we need to swallow these errors
@@ -34,35 +43,40 @@ ServerlessClient.prototype._sleep = delay =>
 
 ServerlessClient.prototype._getIdleProcessesListOrderByDate = async function() {
   return this.query(
-    "SELECT pid,backend_start,state FROM pg_stat_activity WHERE datname='postgres' AND state='idle' ORDER BY backend_start DESC LIMIT 10;"
+    `SELECT pid,backend_start,state 
+        FROM pg_stat_activity 
+        WHERE datname=$1 AND state='idle' 
+        ORDER BY backend_start 
+        DESC LIMIT $2;`,
+    [this._config.database, this._maxRetrivedProcesses]
   );
 };
 
 ServerlessClient.prototype._getProcessesCount = async function() {
   const result = await this.query(
-    "SELECT COUNT(pid) FROM pg_stat_activity WHERE datname='postgres' AND state='idle';"
+    "SELECT COUNT(pid) FROM pg_stat_activity WHERE datname=$1 AND state='idle';",
+    [this._config.database]
   );
 
   return result.rows[0].count;
 };
 
 ServerlessClient.prototype._killProcesses = async function(processesList) {
-  let queries = [];
-  for (const proc of processesList.rows) {
-    queries.push(
-      await this.query(
-        `SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid = '${proc.pid}' AND datname = '${this._config.database}';`
-      )
-    );
-  }
+  const pids = processesList.rows.map(proc => proc.pid);
+  const query = `
+    SELECT pg_terminate_backend(pid) 
+    FROM pg_stat_activity 
+    WHERE pid = ANY ($1) 
+      AND datname = $2 AND state='idle';`
+  const values = [pids, this._config.database]
 
-  return await Promise.all(queries);
+  return this.query(query, values)
 };
 
 ServerlessClient.prototype.clean = async function() {
   const processCount = await this._getProcessesCount();
 
-  if (processCount > 50) {
+  if (processCount > this._maxConnections * this._processesPercentageThreshold) {
     const processesList = await this._getIdleProcessesListOrderByDate();
     await this._killProcesses(processesList);
   }
