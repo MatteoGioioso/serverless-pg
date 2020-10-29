a91f62bdad623c887b9dc560f236d56dad492683
Matteo Gioioso <gioioso.matteo@gmail.com>
2020-10-29 12:55:53 +0700

    Update main class and documentation (#21)
    * feat: update README.md
    
    add intro description and table with the configurations parameters
    
    * chore: add tests
    
    add integration tests to test the main class API and methods
    
    * feat: update main class
    
    - add new default strategy and possibility to choose between differet approaches
    - refine queries
    - adjust count query to query all the connected processes for the given user
    - use state_change instead of backend_start
    
    * feat: update README.md
    
    - fix parameters for config
    
    * chore: add integration tests for API and private methods
    
    * feat: update main class
    
    - fix reconnection for client
    - add query retry
    - add client clean-up
    
    * chore: update package.json
    
    - add jest and husky for test and pre-commit
    - fix licence
    - add husky config

--- README.md
+++ README.md
@@ -1,28 +1,56 @@
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/MatteoGioioso/serverless-pg)
+[![npm version](https://badge.fury.io/js/serverless-postgres.svg)](https://badge.fury.io/js/serverless-postgres)
+![GitHub](https://img.shields.io/github/license/MatteoGioioso/serverless-pg)
 
-## Serverless-postgres
+# Serverless-postgres
 
-Manage PostgreSQL connection inside a serverless function.
+Serverless-postgres is a wrapper for **[node-pg](https://github.com/brianc/node-postgres)** Node.js module.
+
+### Why I need this module?
+In a serverless application a function can scale almost "infinitely" by creating separate container instances 
+for each concurrent user. 
+Each container can correspond to a database connection which, for performance purposes, is left opened for further
+re-utilization. If we have a sudden spike of concurrent traffic, the available connections can be quickly maxed out
+by other competing functions.
+If we reach the max connections limit, Postgres will automatically reject any frontend trying to connect to its backend.
+This can cause heavy disruption in your application.
+
+### What does it do?
+Serverless-postgres adds a connection management component specifically for FaaS based applications.
+By calling the method `.clean()` at the end of your functions, the module will constantly monitor the status of all
+the processes running in the PostgreSQL backend and then, based on the configuration provided, 
+will garbage collect the "zombie" connections.
+If the client fails to connect with `"sorry, too many clients already"` error, the module will retry
+using trusted backoff algorithms.
+
+> **NOTE:** This module *should* work with any PostgreSQL server. 
+It has been tested with AWS's RDS Postgres, Aurora Postgres, and Aurora Serverless.
+
+Feel free to request additional features and contribute =)
+
+## Install
 
 ```bash
 npm i serverless-postgres
 ```
 
-### Usage
+## Usage
 
 Declare the ServerlessClient outside the lambda handler
 
-```js
+```javascript
 const client = new ServerlessClient({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    debug: true,
+    delayMs: 3000,
 });
 
 const handler = async(event, context) => {
-    await client.sconnect();
+    await client.connect();
     const result = await client.query(`SELECT NOW()`);
     await client.clean();
     return {
@@ -33,3 +61,22 @@ const handler = async(event, context) => {
 
 
 ```
+
+
+## Configuration Options
+
+| Property | Type | Description | Default |
+| -------- | ---- | ----------- | ------- |
+| config | `Object` | A `node-pg` configuration object as defined [here](https://node-postgres.com/api/client) | `{}` |
+| maxConnsFreqMs | `Integer` | The number of milliseconds to cache lookups of max_connections. | `60000` |
+| automaticMaxConnections | `Boolean` | if this parameters is set to true it will query to get the maxConnections values, to maximize performance you should set the `maxConnections` yourself | `false` |
+| maxConnections | `Integer` | Max connections of your PostgreSQL. I highly suggest to set this yourself | `100` |
+| strategy | `String` | Name of your chosen strategy for cleaning up "zombie" connections, allowed values `minimum_idle_time` or `ranked` | `minimum_idle_time` |
+| minConnectionIdleTimeSec | `Integer` | The minimum number of seconds that a connection must be idle before the module will recycle it. | `0.5` |
+| maxIdleConnectionsToKill | `Integer` or `null` | The amount of max connection that will get killed. Default is `ALL` | `null` |
+| connUtilization | `Number` | The percentage of total connections to use when connecting to your PostgreSQL server. A value of `0.80` would use 80% of your total available connections. | `0.8` |
+| debug | `Boolean` | Enable/disable debugging logs | `false` |
+| capMs | `Integer` | Maximum number of milliseconds between connection retries. | `1000` |
+| baseMs | `Integer` | Number of milliseconds added to random backoff values. | `2` |
+| delayMs | `Integer` | Additional delay to add to the exponential backoff. | `1000` |
+| maxRetries | `Integer` | Maximum number of times to retry a connection before throwing an error. | `3` |
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * This module wrap node-postgres package, more detail regarding it can be found here:
  * https://github.com/brianc/node-postgres
  * @author Matteo Gioioso <matteo@hirvitek.com>
- * @version 1.1.0
+ * @version 1.3.0
  * @license MIT
  */
 
@@ -12,35 +12,46 @@ const { Client } = require("pg");
 function ServerlessClient(config) {
   this._config = config;
 
-  // If this parameters is set to true it will query to get the maxConnections values,
-  // to maximize performance you should set the maxConnections yourself.
-  // Is suggested to manually set the maxConnections and keep this setting to false.
-  this._automaticMaxConnections = config.automaticMaxConnections
-  // Cache expiration for getting the max connections value in milliseconds
-  this._maxConnsFreqMs = config.maxConnsFreqMs || 60000;
-  this._maxConnections = config.maxConnections || 100;
-  this._maxConnectionsCache = {
-    total: this._maxConnections,
-    updated: Date.now()
+  this._maxConns = {
+    // Cache expiration for getting the max connections value in milliseconds
+    freqMs: config.maxConnsFreqMs || 60000,
+    // If this parameters is set to true it will query to get the maxConnections values,
+    // to maximize performance you should set the maxConnections yourself.
+    // Is suggested to manually set the maxConnections and keep this setting to false.
+    automaticMaxConnections: config.automaticMaxConnections,
+    cache: {
+      total: config.maxConnections || 100,
+      updated: 0
+    }
+  }
+
+  // Strategy
+  this._strategy = {
+    name: config.strategy || 'minimum_idle_time',
+    // The minimum number of seconds that a connection must be idle before the module will recycle it.
+    minConnIdleTimeSec: config.minConnectionIdleTimeSec || 0.5,
+    // The bigger, the more idle connections will be killed
+    // this parameters control how aggressive is going to be your strategy
+    // default is null which will means LIMIT ALL
+    maxIdleConnectionsToKill: config.maxIdleConnectionsToKill || null,
+
+    // The percentage of total connections to use when connecting to your Postgres server.
+    // A value of 0.75 would use 75% of your total available connections.
+    // Past this threshold the connection killer will kick in.
+    connUtilization: config.connUtilization || 0.8
   }
 
   // Activate debugging logger
   this._debug = config.debug
 
-  // The bigger, the more idle connections will be possibly dropped
-  this._maxIdleConnections = config.maxIdleConnections || 10;
-
-  // The percentage of total connections to use when connecting to your Postgres server.
-  // A value of 0.75 would use 75% of your total available connections.
-  // Past this threshold the connection killer will kick in.
-  this._connUtilization = config.connUtilization || 0.8
-
+  // Backoff
   this._backoff = {
-    capMs: config.capMs,
-    baseMs: config.baseMs,
-    delayMs: config.retryDelayMs || 1000,
+    capMs: config.capMs || 1000,
+    baseMs: config.baseMs || 2,
+    delayMs: config.delayMs || 1000,
     maxRetries: config.maxRetries || 3,
     retries: 0,
+    queryRetries: 0
   }
 }
 
@@ -52,99 +63,149 @@ ServerlessClient.prototype._sleep = delay =>
     }, delay);
   });
 
-ServerlessClient.prototype._setMaxConnections = async () => {
+ServerlessClient.prototype._setMaxConnections = async (__self) => {
   // If cache is expired
-  if (Date.now() - this._maxConnectionsCache.updated > this._maxConnsFreqMs) {
-    const results = await this._client.query(
-      `SHOW max_connections`
-    )
+  if (Date.now() - __self._maxConns.cache.updated > __self._maxConns.freqMs) {
+    const results = await __self._client.query(`SHOW max_connections`)
 
-    this._logger("Getting max connections from database...")
+    __self._logger("Getting max connections from database...")
 
-    this._maxConnectionsCache = {
-      total: results.rows[0],
+    __self._maxConns.cache = {
+      total: results.rows[0].max_connections,
       updated: Date.now()
     }
   }
-
-  this._maxConnections = this._maxConnectionsCache.total
 }
 
+// This strategy arbitrarily (maxIdleConnections) terminates connections starting from the oldest one in idle.
+// It is very aggressive and it can cause disruption if a connection was in idle for a short period of time
 ServerlessClient.prototype._getIdleProcessesListOrderByDate = async function() {
-  const result =  await this._client.query(
-    `SELECT pid,backend_start,state 
-        FROM pg_stat_activity 
-        WHERE datname=$1 AND state='idle' 
-        ORDER BY backend_start 
-        DESC LIMIT $2;`,
-    [this._config.database, this._maxIdleConnections]
-  );
+  const query = `
+    SELECT pid,backend_start,state 
+    FROM pg_stat_activity 
+    WHERE datname=$1 
+      AND state='idle' 
+      AND usename=$2
+    ORDER BY state_change 
+    LIMIT $3;`
+
+  const values = [
+    this._config.database,
+    this._config.user,
+    this._strategy.maxIdleConnectionsToKill
+  ]
+
+  const result = await this._client.query(query, values);
 
   return result.rows
 };
 
+// This strategy select only the connections that have been in idle state for more
+// than a minimum amount of seconds, it is very accurate as it only takes the process that have been in idle
+// for more than a threshold time (minConnectionTimeoutSec)
+ServerlessClient.prototype._getIdleProcessesListByMinimumTimeout = async function(){
+  const query = `
+    WITH processes AS(
+      SELECT
+         EXTRACT(EPOCH FROM (Now() - state_change)) AS idle_time,
+         pid
+      FROM pg_stat_activity
+      WHERE usename=$1 
+        AND datname=$2 
+        AND state='idle'
+    )
+    SELECT pid 
+    FROM processes 
+    WHERE idle_time > $3 
+    LIMIT $4;`
+
+  const values = [
+    this._config.user,
+    this._config.database,
+    this._strategy.minConnIdleTimeSec,
+    this._strategy.maxIdleConnectionsToKill
+  ]
+
+  const result = await this._client.query(query, values)
+
+  return result.rows
+}
+
 ServerlessClient.prototype._getProcessesCount = async function() {
-  const result = await this._client.query(
-    "SELECT COUNT(pid) FROM pg_stat_activity WHERE datname=$1 AND state='idle';",
-    [this._config.database]
-  );
+  const query = `
+    SELECT COUNT(pid) 
+    FROM pg_stat_activity 
+    WHERE datname=$1 
+      AND usename=$2;`
+
+  const values = [this._config.database, this._config.user]
+
+  const result = await this._client.query(query, values);
 
   return result.rows[0].count;
 };
 
 ServerlessClient.prototype._killProcesses = async function(processesList) {
   const pids = processesList.map(proc => proc.pid);
+
   const query = `
     SELECT pg_terminate_backend(pid) 
     FROM pg_stat_activity 
-    WHERE pid = ANY ($1) 
-      AND datname = $2 AND state='idle';`
-  const values = [pids, this._config.database]
+    WHERE pid = ANY ($1);`
+
+  const values = [pids]
 
   return this._client.query(query, values)
 };
 
-ServerlessClient.prototype._decorrelatedJitter = function(delay= 0){
-  const cap = Number.isInteger(this._backoff.capMs) ? this._backoff.capMs : 100 // default to 100 ms
-  const base = Number.isInteger(this._backoff.baseMs) ? this._backoff.baseMs : 2 // default to 2 ms
-  const randRange = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min
-  return Math.min(cap, randRange(base, delay * 3))
+ServerlessClient.prototype._getStrategy = function(){
+  switch (this._strategy.name) {
+    case "minimum_idle_time":
+      return this._getIdleProcessesListByMinimumTimeout.bind(this)
+    case "ranked":
+      return this._getIdleProcessesListOrderByDate.bind(this)
+    default:
+      return this._getIdleProcessesListByMinimumTimeout.bind(this)
+  }
+}
+
+ServerlessClient.prototype._decorrelatedJitter = function(delay){
+  const cap = this._backoff.capMs;
+  const base = this._backoff.baseMs;
+  const randRange = (min,max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.min(cap, randRange(base, delay * 3));
 }
 
 ServerlessClient.prototype.clean = async function() {
   const processCount = await this._getProcessesCount();
-  this._logger("Current process count: ", processCount)
+  this._logger("Current process count: ", processCount);
 
-  if (processCount > this._maxConnections * this._connUtilization) {
-    const processesList = await this._getIdleProcessesListOrderByDate();
-    await this._killProcesses(processesList);
-    this._logger("Killed processes: ", processesList.length)
+  if (processCount > this._maxConns.cache.total * this._strategy.connUtilization) {
+    const strategy = this._getStrategy();
+    const processesList = await strategy();
+    const killedProcesses = await this._killProcesses(processesList);
+    this._logger("+++++ Killed processes: ", killedProcesses.rows.length, " +++++")
+    return killedProcesses.rows
   }
 };
 
-ServerlessClient.prototype.sconnect = async function() {
+ServerlessClient.prototype.connect = async function() {
   try {
-    await this._init()
-    await this._client.connect();
+    await this._init();
   } catch (e) {
     if (e.message === "sorry, too many clients already") {
       // Client in node-pg is usable only one time, once it errors we cannot re-connect again,
       // therefore we need to throw the instance and recreate a new one
-      await this._init()
-      const backoff = async delay => {
-        if (this._backoff.retries < this._backoff.maxRetries) {
-          this._logger(this._backoff.maxRetries, " trying to reconnect... ")
-          const totalDelay = this._decorrelatedJitter(delay)
-          this._logger("total delay: ", totalDelay)
-          await this._sleep(totalDelay);
-          this._backoff.retries++;
-          await this.sconnect();
-          this._logger("Re-connection successful after ", this._backoff.retries)
-        }
-      };
-
-      this._logger("Current delay: ", this._backoff.delayMs)
-      await backoff(this._backoff.delayMs);
+      if (this._backoff.retries < this._backoff.maxRetries) {
+        this._logger("trying to reconnect...attempt: ", this._backoff.retries)
+        const totalDelay = this._decorrelatedJitter(this._backoff.delayMs)
+        this._logger("total delay: ", totalDelay)
+        await this._sleep(totalDelay);
+        this._backoff.retries++;
+        await this.connect();
+      } else {
+        throw e
+      }
     } else {
       throw e;
     }
@@ -153,12 +214,6 @@ ServerlessClient.prototype.sconnect = async function() {
 
 ServerlessClient.prototype._init = async function(){
   this._client = new Client(this._config)
-
-  if (this._automaticMaxConnections){
-    await this._setMaxConnections()
-  }
-
-  this._logger("Max connections: ", this._maxConnections)
 
   // pg throws an error if we terminate the connection, therefore we need to swallow these errors
   // and throw the rest
@@ -174,6 +229,15 @@ ServerlessClient.prototype._init = async function(){
       throw err;
     }
   });
+
+  await this._client.connect();
+  this._backoff.retries = 0
+
+  if (this._maxConns.automaticMaxConnections){
+    await this._setMaxConnections(this)
+  }
+
+  this._logger("Max connections: ", this._maxConns.cache.total)
 }
 
 // TODO add validation for the client config
@@ -188,11 +252,37 @@ ServerlessClient.prototype._logger = function(...args) {
 }
 
 ServerlessClient.prototype.query = async function(...args){
-  return this._client.query(...args)
+  try {
+    // We fulfill the promise to catch the error
+    return await this._client.query(...args)
+  } catch (e) {
+    // If a client has been terminated by serverless-postgres and try to query again
+    // we re-initialize it and retry
+    if (e.message === "Client has encountered a connection error and is not queryable"){
+      if (this._backoff.queryRetries < this._backoff.maxRetries) {
+        this._logger("Retry query...attempt: ", this._backoff.queryRetries)
+        const totalDelay = this._decorrelatedJitter(this._backoff.delayMs)
+        this._logger("total delay: ", totalDelay)
+        await this._sleep(totalDelay);
+        this._backoff.queryRetries++;
+        await this.connect()
+        const result = await this.query(...args)
+        this._backoff.queryRetries = 0
+        return result
+      } else {
+        throw e
+      }
+    }
+    throw e
+  }
 }
 
 ServerlessClient.prototype.end = async function(){
-  return this._client.end()
+  this._backoff.retries = 0
+  this._backoff.queryRetries = 0
+  const result = await this._client.end()
+  this._client = null
+  return result
 }
 
 ServerlessClient.prototype.on = function(...args){
